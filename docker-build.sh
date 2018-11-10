6c1380426c3aef4b8b2b18f866c41fda5d73c6a5
Jose Riguera <jriguera@gmail.com>
2018-11-11 00:58:38 +0100

    Update release script

--- docker-build.sh
+++ docker-build.sh
@@ -3,9 +3,23 @@
 DOCKER=docker
 NAME="homeassistant"
 
+ARCH=""
+case "$(uname -m)" in
+    armv7l)
+        ARCH='arm32v6'
+    ;;
+    x86_64|amd64)
+        ARCH='amd64'
+    ;;
+    *)
+        echo "ERROR: unsupported architecture: $(uname -m)"
+        exit 1
+    ;;
+esac
+
 pushd docker
     $DOCKER build \
-      --build-arg  ARCH=$(dpkg --print-architecture) \
+      --build-arg ARCH=${ARCH} \
       --build-arg TZ=$(timedatectl  | awk '/Time zone:/{ print $3 }') \
       .  -t $NAME
 popd

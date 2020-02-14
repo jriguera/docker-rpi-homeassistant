a98a13e33036233c7130a8a0e2f58fb8069d866e
Jose Riguera <jriguera@gmail.com>
2020-02-14 23:16:30 +0100

    HA v 0.105.3

--- docker-build.sh
+++ docker-build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+set -euf -o pipefail
 
 DOCKER=docker
 NAME="homeassistant"
-
+VERSION=$(cd "$(dirname "${BASH_SOURCE[0]}")/home-assistant" && git describe --tags | head -n1)
 ARCH=""
+
 case "$(uname -m)" in
     armv7l)
         ARCH='arm32v6'
@@ -18,6 +20,7 @@ case "$(uname -m)" in
 esac
 
 exec $DOCKER build \
+    --build-arg VERSION=${VERSION} \
     --build-arg ARCH=${ARCH} \
     --build-arg TZ=$(timedatectl  | awk '/Time zone:/{ print $3 }') \
     .  -t $NAME
